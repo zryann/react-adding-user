4fe086ab868a9ed6956e2398e18eab8146fc36fc
Zryan Muhammed Jaafer <zryan.muhammed.jaafer@gmail.com>
2022-12-24 17:10:13 +0300

    User List component created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+  const addUserHandler = (name, age) => {
+    setUsersList((prev) => [
+      ...prev,
+      { name, age, id: Math.random().toString() },
+    ]);
+  };
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </div>
   );
 }
